f118e708c6dae45e8fcbe6c36825c21dba6fd3a1
Steinbach4485 <kadjowsteinbach@gmail.com>
2026-08-06 20:21:26 -0300

    Add files via upload

---
+++ PooUnivel/src/Aula2_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Aula2_1 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = entrada.nextLine();
+
+        System.out.println("Olá " + nome + "!");
+
+        entrada.close();
+    }
+}
