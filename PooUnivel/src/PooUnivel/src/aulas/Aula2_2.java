980b49e09c9db80084c83c865c207333f0d90593
Steinbach4485 <kadjowsteinbach@gmail.com>
2026-08-18 20:26:46 -0300

    Add files via upload

---
+++ PooUnivel/src/PooUnivel/src/aulas/Aula2_2.java
@@ -0,0 +1,35 @@
+package aulas;
+
+import java.util.Scanner;
+
+public class Aula2_2 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+
+        //Declaração das variaveis
+        String nome;
+        int idade;
+        double altura;
+
+
+        System.out.print("Digite seu nome: "); 
+        nome = entrada.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        idade = entrada.nextInt();
+
+        System.out.print("Digite sua altura: ");
+        altura = entrada.nextDouble();
+
+        System.out.println("\n=== DADOS INFORMADOS === ");
+        System.out.println("Nome" + nome);
+        System.out.println("idade: " + idade + "anos");
+        System.out.println("Altura: " + altura + "m");
+
+
+        entrada.close(); 
+    }
+}
